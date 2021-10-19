4f974e103b9664a3a8d04c8354675ccce7a5c729
milan115 <milanlm798@gmail.com>
2021-10-19 18:36:55 +0600

    quize

---
+++ precache-manifest.11868713f9d0555db553273d488b7bea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a9e7c7f6d6808aaa7033485e60dab6",
+    "url": "/quize/index.html"
+  },
+  {
+    "revision": "1728a44910ef0d8be418",
+    "url": "/quize/static/css/main.71d76b12.chunk.css"
+  },
+  {
+    "revision": "b5f0fd39b47b16405721",
+    "url": "/quize/static/js/2.1a2e4dea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quize/static/js/2.1a2e4dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1728a44910ef0d8be418",
+    "url": "/quize/static/js/main.18f22dbb.chunk.js"
+  },
+  {
+    "revision": "f7d5338fb0bda50c958d",
+    "url": "/quize/static/js/runtime-main.c432fa62.js"
+  }
+]);
